e816f98ab06a7f0cfabbd7bcb030c9aecaef2449
Adriano Braga <adriano.braga@dtidigital.com.br>
2020-11-16 23:25:21 -0300

    Testes de post para manipular view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/teste', function(req, res, next) {
+  if(req.body.teste == 'testar') {
+    res.render('index', { title: 'Express', text: 'Testar' });
+  }
+  else {
+    res.render('index', { title: 'Express', text: 'Não testar' });
+  }
+});
+
 module.exports = router;
